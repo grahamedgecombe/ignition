13f23739ab1c91aa7af1e4d2ac7ea2369afd81e6
Graham Edgecombe <grahamedgecombe@gmail.com>
2010-12-16 17:06:08 +0000

    Moved the code which validates the page/path to the router, using a constraint.

--- app/controllers/ignition/pages_controller.rb
+++ app/controllers/ignition/pages_controller.rb
@@ -1,69 +1,29 @@
 require 'ignition'
 
-# A Rails controller which maps static content to actual template files in the
-# <tt>RAILS_ROOT/app/views/pages</tt> directory.
-class Ignition::PagesController < ApplicationController
-  # The cascade header used by the router.
-  X_CASCADE = 'X-Cascade'.freeze
+module Ignition
+  # A Rails controller which maps static content to actual template files in the
+  # <tt>RAILS_ROOT/app/views/pages</tt> directory.
+  class PagesController < ApplicationController
+    # Creates the controller.
+    def initialize
+      super
 
-  # The value in the cascade header which causes the router to pass and try the
-  # next route.
-  PASS = 'pass'.freeze
-
-  # The subdirectory of the app/views directory where static pages are loaded
-  # from. This is set to <tt>'pages'</tt>.
-  PAGES_DIRECTORY = 'pages'.freeze
-
-  # Creates the controller.
-  def initialize
-    super
-
-    caching_method = Ignition::Configuration.caching_method
-    if caching_method == :page
-      caches_page :show
-    elsif caching_method == :page_without_layout
-      caches_action :show, :layout => false
-    elsif caching_method == :none
-      # do nothing
-    else
-      raise "Invalid caching method: #{caching_method}"
-    end
-  end
-
-  # Attempts to show the page specified by the <tt>params[:id]</tt> parameter.
-  def show
-    id = params[:id]
-
-    if not valid_path? id
-      render_404
-    else
-      begin
-        render :template => "#{PAGES_DIRECTORY}/#{id}"
-      rescue ActionView::MissingTemplate
-        render_404
+      case Ignition::Configuration.caching_method
+      when :page then
+        caches_page :show
+      when :page_without_layout then
+        caches_action :show, :layout => false
+      when :none then
+        # do nothing
+      else
+        raise "Invalid caching method: #{caching_method}"
       end
     end
+
+    # Shows the page specified by the <tt>params[:id]</tt> parameter.
+    def show
+      id = params[:id]
+      render :template => "#{Ignition::PAGES_DIRECTORY}/#{id}"
+    end
   end
-
-  private
-    # Checks that the page with the specified <tt>id</tt> is valid and does not
-    # map to a template outside of the <tt>RAILS_ROOT/app/views/pages</tt>
-    # directory. If it does, an exception is thrown.
-    def valid_path?(id)
-      Rails.application.paths.app.views.each do |path|
-        base = File.expand_path(PAGES_DIRECTORY, path)
-        file = File.expand_path(id, base)
-        if not file.starts_with? base
-          return false
-        end
-      end
-      return true
-    end
-
-    # Renders a 404 response with the X-Cascade header set to 'pass'. This
-    # causes the router to carry on trying rules.
-    def render_404
-      headers[X_CASCADE] = PASS
-      render :nothing => true, :status => :not_found
-    end
 end
--- lib/ignition/routing.rb
+++ lib/ignition/routing.rb
@@ -1,6 +1,38 @@
 require 'action_dispatch'
 
 module Ignition
+  # The subdirectory of the app/views directory where static pages are loaded
+  # from. This is set to <tt>'pages'</tt>.
+  PAGES_DIRECTORY = 'pages'.freeze
+
+  # A constraint which checks if the specified page exists.
+  class PageExistsConstraint
+    class << self
+      # Checks if the page specified by the <tt>id</tt> path parameter exists.
+      def matches?(request)
+        id = request.symbolized_path_parameters[:id]
+        return exists_and_valid? id
+      end
+
+      private
+        # Checks that the page with the specified <tt>id</tt> exists and does not
+        # map to a template outside of the <tt>RAILS_ROOT/app/views/pages</tt>
+        # directory.
+        def exists_and_valid?(id)
+          found = false
+          Rails.application.paths.app.views.each do |path|
+            base = File.expand_path(Ignition::PAGES_DIRECTORY, path)
+            file = File.expand_path(id, base)
+            if not file.starts_with? base
+              return false
+            end
+            found = true unless Dir.glob("#{file}.*").empty?
+          end
+          return found
+        end
+    end
+  end
+
   # Extends the default <tt>ActionDispatch::Routing::Mapper</tt> module with a
   # <tt>static</tt> method which creates a route for static content.
   module Mapper
@@ -11,7 +43,9 @@ module Ignition
     # a different name for the route (the default is <tt>'page'</tt>).
     def static(prefix = '/', options = {})
       prefix = prefix.gsub /\/$/, ''
-      get "#{prefix}/*id", :to => 'ignition/pages#show', :as => (options[:as] || 'page')
+      constraints Ignition::PageExistsConstraint do
+        get "#{prefix}/*id", :to => 'ignition/pages#show', :as => (options[:as] || 'page')
+      end
     end
   end
 end
