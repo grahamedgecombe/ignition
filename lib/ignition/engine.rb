7f6caae3da427d89c350551e479dae126a998ceb
Graham Edgecombe <graham@grahamedgecombe.com>
2014-04-09 16:51:08 +0100

    Remove the hack in engine.rb.
    It doesn't seem to be required in 4.0.0 and above.

--- lib/ignition/engine.rb
+++ lib/ignition/engine.rb
@@ -6,21 +6,3 @@ module Ignition
     config.ignition = Ignition::Configuration.new
   end
 end
-
-# this hack allows URLs to be generated correctly if the engine is mounted at /
-class ActionDispatch::Routing::Mapper
-  private
-    alias_method :old_define_generate_prefix, :define_generate_prefix
-
-    def define_generate_prefix(app, name)
-      ret = old_define_generate_prefix(app, name)
-      app.routes.singleton_class.class_eval do
-        alias_method :_old_generate_prefix, :_generate_prefix
-
-        redefine_method :_generate_prefix do |options|
-          prefix = _old_generate_prefix(options)
-          return prefix == '/' ? '' : prefix
-        end
-      end
-    end
-end
